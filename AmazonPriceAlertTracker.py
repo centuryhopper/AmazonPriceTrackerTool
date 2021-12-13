8a21334d39fc2960c2ccf2cd63c353964d528d3d
Leo Zhang <leotheasianlion@gmail.com>
2021-12-13 18:28:20 -0500

    update

--- AmazonPriceAlertTracker.py
+++ AmazonPriceAlertTracker.py
@@ -3,15 +3,17 @@ from bs4 import BeautifulSoup
 import unicodedata
 import pandas as pd
 from prophet import Prophet
-from secrets import Secrets
+# from secrets import Secrets
 from time import strftime
 from collections import defaultdict
-from prophet.plot import plot_plotly, plot_components_plotly
+# from prophet.plot import plot_plotly, plot_components_plotly
+import matplotlib.pyplot as plt
 
 
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
+
 
 #region getProductInfo(url)
 def getProductInfo(url):
@@ -71,10 +73,11 @@ def processAmazonLinks(products):
 if __name__ == '__main__':
     # products = [
     #     ("https://www.amazon.com/QNAP-TS-230-Cortex-A53-Quad-core-Processor/dp/B083W6328Q/ref=sr_1_1?keywords=qnap%2Bts230&qid=1638930694&sr=8-1&th=1", 200),
+    #     ("https://www.amazon.com/gp/product/B07RGPCQG1/ref=as_li_qf_asin_il_tl?ie=UTF8&tag=kallehallde0c-20&creative=9325&linkCode=as2&creativeASIN=B07RGPCQG1&linkId=1f1bbc900d93e4ff3d6ee9e5d16c6092", 220)
     #     ]
     # processAmazonLinks(products)
 
-    df = pd.read_csv('./CSVFiles/dummy.csv')
+    df = pd.read_csv('./dummy.csv')
     df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
     df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
     df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
@@ -82,7 +85,7 @@ if __name__ == '__main__':
     df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
     df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
     df.columns = ['y', 'ds']
-    
+
     # print(df)
     # df['Year'] = df['datetime'].apply(lambda x: str(x)[:4])
     # df['Month'] = df['datetime'].apply(lambda x: str(x)[4:6])
@@ -95,12 +98,13 @@ if __name__ == '__main__':
 
     m = Prophet(interval_width=0.95,daily_seasonality=True)
     model = m.fit(df)
-    future = m.make_future_dataframe(periods=14, freq='D')
-    forecast = m.predict(future)
-    # fig1 = m.plot(forecast)
-    # fig2 = m.plot_components(forecast)
-    plot_plotly(m, forecast)
-    plot_components_plotly(m, forecast)
+    future = m.make_future_dataframe(periods=100, freq='D')
+    forecast_df = m.predict(future)
+    print(forecast_df)
+    fig1 = m.plot(forecast_df)
+    fig2 = m.plot_components(forecast_df)
+    plt.show()
+
 
 
 
