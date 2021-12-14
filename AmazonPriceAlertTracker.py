a61f8bc3f8c8f9e981fc66d96ea572628317c92e
Leo Zhang <leotheasianlion@gmail.com>
2021-12-13 23:00:23 -0500

    update
    added time series machine learning model (still have to collect more data on a daily basis for better predictions)

--- AmazonPriceAlertTracker.py
+++ AmazonPriceAlertTracker.py
@@ -6,12 +6,15 @@ from prophet import Prophet
 from secrets import Secrets
 from time import strftime
 from collections import defaultdict
-from prophet.plot import plot_plotly, plot_components_plotly
+# from prophet.plot import plot_plotly, plot_components_plotly
+import matplotlib.pyplot as plt
+import os
 
 
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
+
 
 #region getProductInfo(url)
 def getProductInfo(url):
@@ -40,67 +43,103 @@ def getProductInfo(url):
 
 #region processAmazonLinks(urls)
 def processAmazonLinks(products):
+#region 
+    def appendToCSV(df,filePath):
+        # if file does not exist write header
+        if not os.path.isfile(filePath):
+            df.to_csv(filePath,index=False)
+        else: # else it exists so append without writing the header
+            df.to_csv(filePath, index=False, mode='a', header=False)
+
+#endregion
+
     curDateTime = strftime("%Y%m%d-%H%M")
-    fileName = f'AmazonPriceAlerts.csv'
-    filePath = f'C:\\Users\\Leo Zhang\\Documents\GitHub\\PythonAmazonPriceTracker\\CSVFiles\\{fileName}'
     products_below_limit = []
     d = defaultdict(list)
-    for product_url, priceLimit in products:
+    for product_title, product_url, priceLimit in products:
         title, price, available = getProductInfo(product_url)
-        # print(title,price,available)
+        fileName = f'{product_title}.csv'
+        filePath = f'C:\\Users\\Leo Zhang\\Documents\GitHub\\PythonAmazonPriceTracker\\CSVFiles\\{fileName}'
         if title is not None and price < priceLimit and available:
             d['title'].append(title)
             d['price'].append(price)
             d['datetime'].append(curDateTime)
             products_below_limit.append((product_url, title, price, priceLimit))
-        # print(d)
-    if products_below_limit:
-        df = pd.DataFrame(d)
-        df.to_csv(filePath, index=False, mode='a', header=False)
-        message = "Subject: Price below limit!\n\n"
-        message += "Your tracked products are below the given limit!\n\n"
-        for url, title, price, priceLimit in products_below_limit:
-            message += f"{title}\n"
-            message +=f"Your customized price limit: ${priceLimit}\n"
-            message += f"Price scraped from online: ${price}\n"
-            message += f"{url}\n\n"
-        Secrets.sendEmail('', '', Secrets.EmailCredentials(sender=Secrets.senderEmail, recipients=Secrets.receiverEmails,password=Secrets.senderEmailPassword), 'Price Drop from Amazon!', message)
+            df = pd.DataFrame(d)
+            appendToCSV(df, filePath)
+            d.clear()
+
+    # TODO: include in the email for when a price drop will occur in the future
+    # if products_below_limit:
+    #     message = "Subject: Price below limit!\n\n"
+    #     message += "Your tracked products are below the given limit!\n\n"
+    #     for url, title, price, priceLimit in products_below_limit:
+    #         message += f"{title}\n"
+    #         message +=f"Your customized price limit: ${priceLimit}\n"
+    #         message += f"Price scraped from online: ${price}\n"
+    #         message += f"{url}\n\n"
+    #     Secrets.sendEmail('', '', Secrets.EmailCredentials(sender=Secrets.senderEmail, recipients=Secrets.receiverEmails,password=Secrets.senderEmailPassword), 'Price Drop from Amazon!', message)
+
 
 #endregion
 
 if __name__ == '__main__':
-    # products = [
-    #     ("https://www.amazon.com/QNAP-TS-230-Cortex-A53-Quad-core-Processor/dp/B083W6328Q/ref=sr_1_1?keywords=qnap%2Bts230&qid=1638930694&sr=8-1&th=1", 200),
-    #     ]
+    products = [
+    ("qnap_network_drive","https://www.amazon.com/QNAP-TS-230-Cortex-A53-Quad-core-Processor/dp/B083W6328Q/ref=sr_1_1?keywords=qnap%2Bts230&qid=1638930694&sr=8-1&th=1", 200),
+    ("portable_monitor","https://www.amazon.com/gp/product/B07RGPCQG1/ref=as_li_qf_asin_il_tl?ie=UTF8&tag=kallehallde0c-20&creative=9325&linkCode=as2&creativeASIN=B07RGPCQG1&linkId=1f1bbc900d93e4ff3d6ee9e5d16c6092", 220),
+    ]
     # processAmazonLinks(products)
 
-    df = pd.read_csv('./CSVFiles/dummy.csv')
-    df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
-    df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
-    df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
-    df['ds'] = pd.DatetimeIndex(df['Year']+'-'+df['Month']+'-'+df['Day'])
-    df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
-    df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
-    df.columns = ['y', 'ds']
-    
-    # print(df)
-    # df['Year'] = df['datetime'].apply(lambda x: str(x)[:4])
-    # df['Month'] = df['datetime'].apply(lambda x: str(x)[4:6])
-    # df['Day'] = df['datetime'].apply(lambda x: str(x)[6:8])
+    #? Note that the csv files must have as many different dates as possible. If all the dates are the same, then there wont be any predictions
+
+    for title,_,_ in products:
+        filePath = f'C:\\Users\\Leo Zhang\\Documents\GitHub\\PythonAmazonPriceTracker\\CSVFiles\\{title}.csv'
+        if not os.path.isfile(filePath): continue
+        df = pd.read_csv(f'./CSVFiles/{title}.csv')
+        df['Year'] = df['datetime'].apply(lambda x: str(x)[:4])
+        df['Month'] = df['datetime'].apply(lambda x: str(x)[4:6])
+        df['Day'] = df['datetime'].apply(lambda x: str(x)[6:8])
+        df['ds'] = pd.DatetimeIndex(df['Year']+'-'+df['Month']+'-'+df['Day'])
+        # axis = 1 for columns, 0 for rows
+        df.drop(['datetime','Year','Month','Day', 'title'], axis=1, inplace=True)
+        df.rename(columns={'price':'y'}, inplace=True)
+        df.reset_index(drop=True,inplace=True)
+        print(df)
+        print(df.dtypes)
+
+        m = Prophet(weekly_seasonality=True)
+        model = m.fit(df)
+        # forecast 14 days into the future
+        future = m.make_future_dataframe(periods=14)
+        forecast_df = m.predict(future)
+        print(forecast_df.head(10))
+        print(forecast_df.tail(10))
+        print()
+
+#region test code
+    # df = pd.read_csv('./CSVFiles/dummy.csv')
+    # df['Year'] = df['Time Date'].apply(lambda x: str(x)[-4:])
+    # df['Month'] = df['Time Date'].apply(lambda x: str(x)[-6:-4])
+    # df['Day'] = df['Time Date'].apply(lambda x: str(x)[:-6])
     # df['ds'] = pd.DatetimeIndex(df['Year']+'-'+df['Month']+'-'+df['Day'])
-    # # axis = 1 for columns, 0 for rows
-    # df.drop(['datetime','Year','Month','Day', 'title'], axis=1, inplace=True)
-    # df.rename(columns={'price':'y'}, inplace=True)
+    # df = df.loc[(df['Product']==2667437) & (df['Store']=='QLD_CW_ST0203')]
+    # df.drop(['Time Date', 'Product', 'Store', 'Year', 'Month', 'Day'], axis=1, inplace=True)
+    # df.columns = ['y', 'ds']
+
     # print(df)
 
-    m = Prophet(interval_width=0.95,daily_seasonality=True)
-    model = m.fit(df)
-    future = m.make_future_dataframe(periods=14, freq='D')
-    forecast = m.predict(future)
-    # fig1 = m.plot(forecast)
-    # fig2 = m.plot_components(forecast)
-    plot_plotly(m, forecast)
-    plot_components_plotly(m, forecast)
+    # m = Prophet(interval_width=0.95,daily_seasonality=True)
+    # model = m.fit(df)
+    # # forecast 365 days into the future
+    # future = m.make_future_dataframe(periods=365, freq='D')
+    # forecast_df = m.predict(future)
+    # print(forecast_df.head(10))
+    # print(forecast_df.tail(10))
+    # fig1 = m.plot(forecast_df)
+    # fig2 = m.plot_components(forecast_df)
+    # plt.show()
+
+#endregion
 
 
 
